b99af256e6c1fa1de9fe7504c563ab9ea7bb30d7
Abdulhamidfrontedd <abdulhamidturgunov1606@gmail.com>
2023-04-02 08:36:21 +0500

    key-events

---
+++ script.js
@@ -0,0 +1,14 @@
+let opend = document.querySelector('#opened');
+let keys = document.querySelector('.keys');
+let code = document.querySelector('#code');
+let keyCode = document.querySelector('#keyCode');
+let key = document.querySelector('#key');
+
+
+document.addEventListener('keydown', (e) =>{
+    opend.classList.add('hidden')
+    keys.classList.remove('hidden')
+    code.textContent = e.code
+    keyCode.textContent = e.keyCode
+    key.textContent = e.key
+})
